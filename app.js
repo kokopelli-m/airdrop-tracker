f9942b57a4b06b907b19386fea2140f29d3537cd
Muge | Kokopelli <111564483+kokopelli-m@users.noreply.github.com>
2026-04-02 19:24:24 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+let projects = JSON.parse(localStorage.getItem("projects")) || [];
+
+function save() {
+  localStorage.setItem("projects", JSON.stringify(projects));
+}
+
+function render() {
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+
+  projects.forEach((p, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${p.name} - ${p.status}
+      <button onclick="removeProject(${index})">X</button>
+    `;
+    list.appendChild(li);
+  });
+}
+
+function addProject() {
+  const name = document.getElementById("name").value;
+  const status = document.getElementById("status").value;
+
+  if (!name) return;
+
+  projects.push({ name, status });
+  save();
+  render();
+}
+
+function removeProject(index) {
+  projects.splice(index, 1);
+  save();
+  render();
+}
+
+render();
